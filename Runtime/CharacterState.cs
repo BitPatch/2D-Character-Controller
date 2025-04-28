0d68484646a867490a68e83753b61a5087fe02b8
Denis Kondratev <53820149+denis-kondratev@users.noreply.github.com>
2025-04-28 22:51:02 +0200

    Add character body and state

---
+++ Runtime/CharacterState.cs
@@ -0,0 +1,19 @@
+namespace Bitpatch.CharacterController
+{
+
+    /// <summary>
+    /// Describes the state of <see cref="CharacterBody"/>.
+    /// </summary>
+    public enum CharacterState
+    {
+        /// <summary>
+        /// The character stays steady on the ground and can move freely along it.
+        /// </summary>
+        Grounded,
+
+        /// <summary>
+        /// The character is in a state of free fall.
+        /// </summary>
+        Airborne
+    }
+}
